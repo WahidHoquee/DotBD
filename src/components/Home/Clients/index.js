cb819b7915ac0e3ea35a261cd795349684c50c02
Wahid Hoque <wahidhoquee@gmail.com>
2020-11-26 02:26:42 +0600

    responsive: home > client

--- src/components/Home/Clients/index.js
+++ src/components/Home/Clients/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from "gatsby-image"
-import { Container, Grid } from '@material-ui/core';
+import { useMediaQuery, Container, Grid } from '@material-ui/core';
 
 import styles from './index.module.scss'
 import Title from '../../Utils/Title'
@@ -8,24 +8,26 @@ import useClientImage from '../../../hooks/useClientImage';
 
 const Clients = () => {    
     const clientImage = useClientImage();
+    const sm = useMediaQuery(theme => theme.breakpoints.down(800));
+    const xs = useMediaQuery(theme => theme.breakpoints.down(600));
+    const xxs = useMediaQuery(theme => theme.breakpoints.down(400));
+
     return (
-        <Container fixed style={{padding: '10rem'}}>
-            <Title>Our Clients</Title>
-            <Grid container style={{padding:'5rem'}} justify="center" alignItems="center">
-                {
-                    clientImage.map(client => 
-                        <Grid item sm={3} key={client.base}>
-                            <div className={styles.root}>
-                                <div className={styles.clientBox} >
-                                    <Image fixed={client.image.fixed} alt={client.base} className={styles.logo}/> 
-                                    <p className={styles.location}>Bangladesh</p>
-                                </div>
-                            </div>
-                        </Grid>
-                    )
-                }
+    <Container fluid className={styles.wrapper}>
+        <Title>Our Clients</Title>
+        <Grid container className={styles.root} justify="center" alignItems="center">
+        {clientImage.map(client => 
+            <Grid item xs={xs ? 6 : sm ? 4 : 3} key={client.base} className={styles.card}>
+                <div className={styles.cardClient}>
+                    <div className={styles.clientBox} >
+                        <Image fixed={client.image.fixed} alt={client.base} className={styles.logo}/> 
+                        <p className={styles.location}>Bangladesh</p>
+                    </div>
+                </div>
             </Grid>
-        </Container>
+        )}
+        </Grid>
+    </Container>
     )
 }
 
